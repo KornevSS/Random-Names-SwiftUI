497d43531533e1999477a09a71faeda204fe172f
S.Kornev <post.kornev@gmail.com>
2021-04-08 19:50:49 +0300

    Initial Commit

---
+++ Random Names/Random_NamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_NamesApp.swift
+//  Random Names
+//
+//  Created by Сергей Корнев on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Random_NamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
